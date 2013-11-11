ec406320acc57df7594dae2fd98e37bd9aeac598
geekcheng <geekcheng@gmail.com>
2013-11-11 16:25:45 +0800

    初始化

---
+++ myFuse/src/com/client9/dao/SearchException.java
@@ -0,0 +1,13 @@
+package com.client9.dao;
+
+/**
+ *
+ * @author jgarcia
+ */
+public class SearchException extends RuntimeException {
+
+    public SearchException(Throwable ex) {
+        super(ex);
+    }
+
+}
